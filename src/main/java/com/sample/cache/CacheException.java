087db0d7789215af1de66e56199c9554bd507221
Sandeep Reddy <sandeep.putta.reddy@gmail.com>
2021-07-27 00:02:12 +0800

    Adding Comment

--- src/main/java/com/sample/cache/CacheException.java
+++ src/main/java/com/sample/cache/CacheException.java
@@ -1,7 +1,7 @@
 package com.sample.cache;
 
 public class CacheException extends RuntimeException{
-	
+	//Comment
 	private static final long serialVersionUID = 1L;
 
 	CacheException(String message, Exception e){
